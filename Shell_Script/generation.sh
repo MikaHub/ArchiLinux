08935f95d9f21b968cd46d7e9c5a36a9a1b0ded2
MikaHub <micael.dossantos@edu.itescia.fr>
2020-01-17 11:29:15 +0100

    Code boubacar sur les fichiers vu à la présentation

--- Shell_Script/generation.sh
+++ Shell_Script/generation.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
-$1;
-$2;
-$3;
-$4;
-
-fake='dossantosalmeidamicael';
-
-
+#the arguments
+echo $1;
+echo $2;
+echo $3;
+echo $4;
+#user informations
+fake='yateraboubacar';
+id > uid.txt
+echo "the user id : "
+cat uid.txt | cut -d " " -f1
+echo "the current user : $USER"
+##here we are checking if the users are the sames
 if [ $USER = $fake ]
+##if the users are the sames, you create a directory and put these two files...
 then
+  ##create directory with a name's argument
     mkdir $1;
-    
-    cd /Users/dossantosalmeidamicael/Documents/Archi\ Linux/ArchiLinux;
-
+  ##go to the end directory
+    cd /Users/yateraboubacar/Documents/ArchiLinux;
     ###AN OTHER WAY TO CHANGE REPOS...
     #./genTickExec $1 | ./genSensorExec >> $2 2>> $3 | mkdir $4 | mv $2 $3 $4 CECI MARCHE ROMUALD PAS COMME TA METHODE TCHHHIPPPS
-
     ##Other way to filter the file...
     #./generation.sh hello 100 log.txt err.txt | cat hello/log.txt | cut -d "," -f1 -f2 -f3 -f6 > hello/text.txt
-
-    ./genTickExec $2 | ./genSensorExec  >>  Shell_Script/$1/$3 2>>  Shell_Script/$1/$4  
-
-        ##./genTickExec $2 | ./genSensorExec  >>  Shell_Script/$1/$3 2>>  Shell_Script/$1/$4  
-
+    ##run exec files and write the data in files with arguments
+    cd | ./genTickExec $2 | ./genSensorExec  >>  Shell_Script/$1/$3 2>>  Shell_Script/$1/$4
+    #go to Shell_Script directory
+    cd Shell_Script/
+    #that's for us(listen directory)
+    ls
+    ##move files in home directory (user directory)
+    mv $1   /Users/$USER/$1
+        ##./genTickExec $2 | ./genSensorExec  >>  Shell_Script/$1/$3 2>>  Shell_Script/$1/$4
+## condition
 else
-    echo "Error user"
+    echo "Pas le bon"
+##end
 fi
--- Shell_Script/supervision.sh
+++ Shell_Script/supervision.sh
@@ -1,6 +1,39 @@
-$1;
-$2;
-$3;
-$4;
-$5;
-
+#!/bin/bash
+#the arguments
+echo $1;
+echo $2;
+echo $3;
+echo $4;
+echo $5;
+#user informations
+fake='yateraboubacar';
+id > uid.txt
+echo "the user id : "
+cat uid.txt | cut -d " " -f1
+echo "the current user : $USER"
+##here we are checking if the users are the sames
+if [ $USER = $fake ]
+##if the users are the sames, you create a directory and put these two files...
+then
+  ##create directory with a name's argument
+    mkdir $1;
+  ##go to the end directory
+    cd /Users/yateraboubacar/Documents/ArchiLinux;
+    ###AN OTHER WAY TO CHANGE REPOS...
+    #./genTickExec $1 | ./genSensorExec >> $2 2>> $3 | mkdir $4 | mv $2 $3 $4 CECI MARCHE ROMUALD PAS COMME TA METHODE TCHHHIPPPS
+    ##Other way to filter the file...
+    #./generation.sh hello 100 log.txt err.txt | cat hello/log.txt | cut -d "," -f1 -f2 -f3 -f6 > hello/text.txt
+    ##run exec files
+     ./genTickExec $2 | ./genSensorExec  >>  Shell_Script/$1/$3 2>>  Shell_Script/$1/$4
+     #go to Shell_Script directory
+    cd Shell_Script/
+    #ls
+    ##move files in home directory (user directory)
+    mv $1   /Users/$USER/$1
+    #zip the directory with datetime as directory name
+    tar -cvf $1-$(date +"%Y-%m-%d").tar $1/
+    ##./genTickExec $2 | ./genSensorExec  >>  Shell_Script/$1/$3 2>>  Shell_Script/$1/$4
+##condition
+else
+    echo "Pas le bon"
+fi
